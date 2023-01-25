41dd2fe98c5ebfe0f014a23c32d3bce4bc040767
Austin Law <austinlaw8@gmail.com>
2023-01-25 10:59:55 -0800

    Improves note placement

--- src/NotePlacer.js
+++ src/NotePlacer.js
@@ -1,5 +1,3 @@
-  
-
 import React from 'react';
 
 function NotePlacer(props) {
@@ -10,12 +8,25 @@ function NotePlacer(props) {
         songLength,
     } = props;
     
+
+    // Full width, including the scroll part
+    const fullWidth = Math.max(
+        document.body.scrollWidth,
+        document.documentElement.scrollWidth,
+        document.body.offsetWidth,
+        document.documentElement.offsetWidth,
+        document.body.clientWidth,
+        document.documentElement.clientWidth
+    );
+    console.log(fullWidth)
+        
     return(
         <div style={{position:"relative"}}>
             <hr/>
             <button onClick={ () => addNotes(index) }/>
             { notes.map( (note, i) => {
-                const position = String(Math.floor(note / songLength * 100)) + "%";
+                const position = String(Math.floor((note / songLength + 16 / fullWidth) * 100000) / 1000) + "%";
+                console.log(position)
                 return (
                     <button key={i} style={{position: "absolute", top:"0", left:position}}/>
                 );
